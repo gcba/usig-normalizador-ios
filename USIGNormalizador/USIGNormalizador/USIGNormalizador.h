1d7cfbd9a57364ccaa5b218ac4639ef3b84adb33
Rita Zerrizuela <zeta@widcket.com>
2017-09-28 17:58:22 -0300

    Created basic project

---
+++ USIGNormalizador/USIGNormalizador/USIGNormalizador.h
@@ -0,0 +1,19 @@
+//
+//  USIGNormalizador.h
+//  USIGNormalizador
+//
+//  Created by Rita Zerrizuela on 9/28/17.
+//  Copyright © 2017 GCBA. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for USIGNormalizador.
+FOUNDATION_EXPORT double USIGNormalizadorVersionNumber;
+
+//! Project version string for USIGNormalizador.
+FOUNDATION_EXPORT const unsigned char USIGNormalizadorVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <USIGNormalizador/PublicHeader.h>
+
+
