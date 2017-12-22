07f9545e41956e9f6d89a04ff42fb68bd75650b9
Rita Zerrizuela <zeta@widcket.com>
2017-12-22 10:15:18 -0300

    Added basic EPOk test case

--- USIGNormalizadorTests/USIGNormalizadorTests.swift
+++ USIGNormalizadorTests/USIGNormalizadorTests.swift
@@ -7,6 +7,7 @@
 //
 
 import XCTest
+import Moya
 @testable import USIGNormalizador
 
 class USIGNormalizadorTests: XCTestCase {
@@ -18,7 +19,7 @@ class USIGNormalizadorTests: XCTestCase {
         super.tearDown()
     }
     
-    func testSearch() {
+    func testNormalizadorSearch() {
         let expect = expectation(description: "Se busca una calle y se obtiene una lista de resultados")
         let timeout = 5.0
         
@@ -42,7 +43,7 @@ class USIGNormalizadorTests: XCTestCase {
         }
     }
     
-    func testSearchWithMax() {
+    func testNormalizadorSearchWithMax() {
         let max = 5
         let expect = expectation(description: "Se busca una calle y se obtiene una lista de no más de \(max) resultados")
         let timeout = 5.0
@@ -67,7 +68,7 @@ class USIGNormalizadorTests: XCTestCase {
         }
     }
     
-    func testSearchWithNumber() {
+    func testNormalizadorSearchWithNumber() {
         let expect = expectation(description: "Se busca una calle y altura específicas y se obtiene un resultado")
         let timeout = 5.0
         
@@ -97,7 +98,7 @@ class USIGNormalizadorTests: XCTestCase {
         }
     }
     
-    func testLocation() {
+    func testNormalizadorLocation() {
         let expect = expectation(description: "Se obtiene la localización en base a un par de coordenadas")
         let timeout = 5.0
         
@@ -120,6 +121,45 @@ class USIGNormalizadorTests: XCTestCase {
         waitForExpectations(timeout: timeout) { error in
             if let error = error {
                 XCTFail("Falló waitForExpectations(timeout: \(timeout)) al intentar obtener la localización: \(error.localizedDescription)")
+            }
+        }
+    }
+    
+    func testEpokApiSearch() {
+        let request = USIGEpokAPI.buscar(texto: "aeroparque", categoria: nil, clase: nil, boundingBox: nil, start: nil, limit: nil, total: nil)
+        let api = MoyaProvider<USIGEpokAPI>()
+        let expect = expectation(description: "Se obtienen los detalles de un lugar")
+        let timeout = 5.0
+        let parseError = "Error al parsear la respuesta de la API de lugares"
+        
+        api.request(request, completion: { response in
+            XCTAssert(response.error == nil)
+            XCTAssert(response.value != nil)
+            
+            guard let json = try? response.value?.mapJSON(failsOnEmptyData: true) as? [String: Any],
+                let totalString = json?["totalFull"] as? String,
+                let clases = json?["clasesEncontradas"] as? Array<[String: String]>,
+                let instancias = json?["instancias"] as? Array<[String: String]>,
+                let limitString = json?["total"] as? String,
+                let total = Int(totalString),
+                let limit = Int(limitString) else {
+                XCTFail(parseError)
+                    
+                return
+            }
+            
+            XCTAssert(total > 0)
+            XCTAssert(clases.count > 0)
+            XCTAssert(instancias.count > 0)
+            XCTAssert(limit > 0)
+            
+            expect.fulfill()
+        })
+        
+        
+        waitForExpectations(timeout: timeout) { error in
+            if let error = error {
+                XCTFail("Falló waitForExpectations(timeout: \(timeout)) al intentar buscar un lugar: \(error.localizedDescription)")
             }
         }
     }
