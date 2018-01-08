58ec590d63c7f9b6670338c38a00cbf69e9619fe
Rita <zeta@widcket.com>
2018-01-08 13:35:06 -0300

    Resolved conflicts

--- USIGNormalizador/USIGNormalizador.swift
+++ USIGNormalizador/USIGNormalizador.swift
@@ -24,60 +24,41 @@ public class USIGNormalizador {
     public class func search(query: String, excluding: String? = USIGNormalizadorExclusions.AMBA.rawValue, maxResults: Int = 10,
         completion: @escaping ([USIGNormalizadorAddress]?, USIGNormalizadorError?) -> Void) {
         let request = USIGNormalizadorAPI.normalizar(direccion: query, excluyendo: excluding, geocodificar: true, max: maxResults)
-
+        
         api.request(request) { response in
             let defaultError = "Error calling USIG API"
-
+            
             if let error = response.error, let errorMessage = error.errorDescription {
                 completion(nil, USIGNormalizadorError.other(errorMessage, response.value?.statusCode, response.value?.response))
-
+                
                 return
             }
-
+            
             guard let json = try? response.value?.mapJSON(failsOnEmptyData: false) as? [String: Any] else {
                 completion(nil, USIGNormalizadorError.other(defaultError, response.value?.statusCode, response.value?.response))
-
+                
                 return
             }
-
+            
             if let message = json?["errorMessage"] as? String {
                 if message.lowercased().contains("calle inexistente") || message.lowercased().contains("no existe a la altura") {
                     completion(nil, USIGNormalizadorError.streetNotFound("Street not found", response.value?.statusCode, response.value?.response))
-
+                    
                     return
                 }
                 else {
                     completion(nil, USIGNormalizadorError.service("\(message)", response.value?.statusCode, response.value?.response))
-
+                    
                     return
                 }
             }
-
+            
             guard let addresses = json?["direccionesNormalizadas"] as? Array<[String: Any]>, addresses.count > 0 else {
                 completion(nil, USIGNormalizadorError.other(defaultError, response.value?.statusCode, response.value?.response))
-
+                
                 return
             }
-
-            for item in addresses {
-                let coordinates = item["coordenadas"] as? [String: Any]
-                let latitude = USIGNormalizador.parseCoordinate(fromDict: coordinates, key: "y")
-                let longitude = USIGNormalizador.parseCoordinate(fromDict: coordinates, key: "x")
-
-                let address = USIGNormalizadorAddress(
-                    address: (item["direccion"] as! String).trimmingCharacters(in: .whitespacesAndNewlines).uppercased(),
-                    street: (item["nombre_calle"] as! String).trimmingCharacters(in: .whitespacesAndNewlines),
-                    number: item["altura"] as? Int,
-                    type: (item["tipo"] as! String).trimmingCharacters(in: .whitespacesAndNewlines),
-                    corner: (item["nombre_calle_cruce"] as? String)?.trimmingCharacters(in: .whitespacesAndNewlines),
-                    latitude: latitude,
-                    longitude: longitude,
-                    districtCode: (item["cod_partido"] as? String)?.trimmingCharacters(in: .whitespacesAndNewlines)
-                )
-
-                result.append(address)
-            }
-
+            
             completion(USIGNormalizador.getAddresses(addresses), nil)
         }
     }
